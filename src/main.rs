fda3d42af9e1d75178ae44f1d11cf613c1713bb9
forestaa <t.daichi.3@gmail.com>
2023-05-06 14:04:07 +0900

    freestanding-rust-binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+/// この関数はパニック時に呼ばれる
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
