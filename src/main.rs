aee77909cbda439894c13aa764a7e7461acdf284
forestaa <t.daichi.3@gmail.com>
2023-08-06 21:30:00 +0900

    double-fault-exceptions

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,12 @@ pub extern "C" fn _start() -> ! {
 
     writing_an_os_in_rust::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
+    // uncomment line below to trigger a stack overflow
+    // stack_overflow();
 
     #[cfg(test)]
     test_main();
